cdbaa841b135e7b7686ff4aa5b8c2e482a70da32
gabrielcorreasx <127886700+gabrielcorreasx@users.noreply.github.com>
2023-07-06 14:48:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+
+
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true, video:false});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/mM_W_b-m7/model.json', { probabilityThreshold: 0.7 } ,modelReady);
+}
+
+function modelReady(){
+  classifier.classify(gotResults); 
+}
+
+
